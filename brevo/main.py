9219552d91fb9a7a358e69deadc53a7276d31be4
mate123 <matekopaliani12@gmail.com>
2025-07-19 12:21:44 +0400

    simple ui

--- brevo/main.py
+++ brevo/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from .router import router
 
 app = FastAPI(title="Brevo API Service")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def read_index():
+    return FileResponse('static/index.html')
 
 app.include_router(router)
